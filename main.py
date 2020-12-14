400521d8927a87ea783c41f8f30a113e6e99b23d
1bilalado <1bilalado@gmail.com>
2020-12-14 10:23:50 +0300

    bottom nagivation

--- main.py
+++ main.py
@@ -2,17 +2,56 @@
 # pip3 version 20.1.1
 # interpreter python 3.7
 # kivy => pip install kivy
+# kivyMD => pip install kivy
 # run
 
 import kivy
-from kivy.app import App
-from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivymd.app import MDApp
 kivy.require("2.0.0")
 
+# bottom navigation
+screen_helper = """
+Screen:
+    BoxLayout:
+        orientation: 'vertical'
+        MDToolbar:
+            halign: 'center'
+            title: 'Chatty'
+            md_bg_color: .2, .2, .2, 1
+            specific_text_color: 1, 1, 1, 1
+            
+        MDBottomNavigation:
+            panel_color: .2, .2, .2, 1
+            
+            MDBottomNavigationItem:
+                name: 'screen 1'
+                text: 'explore'
+                MDLabel:
+                    text: 'Explore'
+                    halign: 'center'
+    
+            MDBottomNavigationItem:
+                name: 'screen 2'
+                text: 'chat'
+                MDLabel:
+                    text: 'Chat'
+                    halign: 'center'
+    
+            MDBottomNavigationItem:
+                name: 'screen 3'
+                text: 'profile'
+                MDLabel:
+                    text: 'profile'
+                    halign: 'center'
 
-class Chatty(App):
+"""
+
+
+class Chatty(MDApp):
     def build(self):
-        return Label(text="Hello World")
+        screen = Builder.load_string(screen_helper)
+        return screen
 
 
 if __name__ == '__main__':
