4530e27f6799549b53ad13736599d48904ed228b
1bilalado <1bilalado@gmail.com>
2021-01-06 04:42:19 +0300

    frontend going strong

--- chat_page.py
+++ chat_page.py
@@ -1,41 +1,94 @@
 
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.app import MDApp
+from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
 
-screen_helper = """
+the_chat_page = """
 Screen:
+    window_manager: window_manager
+    
     NavigationLayout:
         ScreenManager:
+            
+            # this is out chat screen.
             Screen:
                 BoxLayout:
                     orientation: 'vertical'
                     MDToolbar:
-                        title: 'chats'
+                        title: "chat"
                         left_action_items: [["menu", lambda x: nav_drawer.set_state()]]
                         elevation: 5
                     Screen:
+                        canvas:
+                            Color:
+                                rgb: 1, 0, 0        
                         MDLabel:
                             text: 'messaging here!'
                             halign: 'center'
+                
+                            
+# this is the panel that pops out of the left                   
         MDNavigationDrawer:
             id: nav_drawer
-            Screen:
+
+            MDScreen:
                 BoxLayout:
                     orientation: 'vertical'
-                    MDToolbar:
-                        title: 'private/group'
-                    Screen:
-                        MDLabel:
-                            text: 'ongoing chats'
-                            halign: 'center'
-            
+                    # choose chat list (personal/private)
+                    
+                    BoxLayout:
+                        orientation: 'horizontal'
+                        size_hint_y: 0.1
+                        padding: 8
+                        
+                        Button:
+                            text: 'private'
+                            on_press:
+                                root.window_manager.current = "private_window"
+                        Button:
+                            on_press:
+                                root.window_manager.current = "group_window"
+                            text: 'group'
+                
+                    # to view selected chat list
+                    WindowManager:
+                        id: window_manager
+                        PrivateWindow:
+                            name: "private_window"
+                        GroupWindow:
+                            name: "group_window"
+                            
+<PrivateWindow>:
+    MDLabel:
+        text: 'it works but wont switch!'
+        halign: 'center'
+<GroupWindow>:
+    MDLabel:
+        text: 'it works!'
+        halign: 'center'                                          
 """
 
 
-class Chatty(MDApp):
+# private chats
+class PrivateWindow(Screen):
+    pass
+# group chats
+class GroupWindow(Screen):
+    pass
+# annoying
+class WindowManager(ScreenManager):
+    pass
+
+# the page
+sm = ScreenManager()
+sm.add_widget(PrivateWindow(name='private'))
+sm.add_widget(GroupWindow(name='group'))
+
+
+class Chatty(MDApp,App):
     def build(self):
-        screen = Builder.load_string(screen_helper)
+        screen = Builder.load_string(the_chat_page)
         return screen
 
 
